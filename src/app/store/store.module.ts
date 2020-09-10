c9cc13401bba8c7ec8e99d7328cc9709ded98c4d
Rodolphe <dutchr@live.fr>
2020-09-10 13:13:28 +0200

    done custom directive

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store/store.component';
 import { ModelModule } from '../model/model.module';
-import { FormsModule } from '@angular/forms'
-
-
+import { FormsModule } from '@angular/forms';
+import { CounterDirective } from './counter.directive';
+ 
 
 @NgModule({
-  declarations: [StoreComponent],
-  imports: [
-    CommonModule, ModelModule, BrowserModule, FormsModule,
-  ],
-  exports:[StoreComponent]
+  declarations: [StoreComponent, CounterDirective],
+  imports: [CommonModule, ModelModule, BrowserModule, FormsModule],
+  exports: [StoreComponent],
 })
-export class StoreModule { }
+export class StoreModule {}
