64ad8dd12738b9a3372d4764777f0089a9a391d7
Rodolphe <dutchr@live.fr>
2020-09-11 09:39:26 +0200

    connecting app & json-server to dispaly real datas

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,8 +4,17 @@ import { StaticDataSource } from './static.datasource';
 import { Cart } from './cart.model';
 import { OrderRepository } from './order.repository';
 import { Order } from './order.model';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RestDataSource } from './rest.datasource';
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart, OrderRepository, Order],
+  providers: [
+    ProductRepository,
+    StaticDataSource,
+    Cart,
+    OrderRepository,
+    Order,
+    { provide: StaticDataSource, useClass: RestDataSource },
+  ],
+  imports: [HttpClientModule],
 })
 export class ModelModule {}
